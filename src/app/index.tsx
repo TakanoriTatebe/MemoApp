4e44260dbb8d6a7c1275ab0e78098ddda03a9900
TakanoriTatebe <takanori.tatebe.0727@gmail.com>
2025-08-06 14:50:55 +0900

    Style header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,14 +3,14 @@ import { Text, View, StyleSheet } from 'react-native'
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
+
       <View>
-
         <View>
           <View>
             <Text>買い物リスト</Text>
@@ -40,7 +40,6 @@ const Index = (): JSX.Element => {
             <Text>X</Text>
           </View>
         </View>
-
       </View>
 
       <View>
@@ -53,8 +52,28 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  header: {
+    backgroundColor: '#467fd3',
+    height: 104,
+    justifyContent:'flex-end'
+  },
+  headerInner: {
     alignItems: 'center'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255,255,255,0.7)'
   }
 })
 
