45e364cacfb52e1c2a4d0f1740ddc83519e61b78
TakanoriTatebe <takanori.tatebe.0727@gmail.com>
2025-08-07 17:33:56 +0900

    Create Auth

--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,29 +1,31 @@
-import { StyleSheet, Text, View} from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+import Icon from "./Icon";
 
 const MemoListItem = (): JSX.Element => {
   return (
-   <View style={styles.memoListItem}>
-    <View>
-      <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-      <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
+    <View style={styles.memoListItem}>
+      <View>
+        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
+      </View>
+      <TouchableOpacity>
+        <Icon name="delete" size={40} color="#b0b0b0" />
+      </TouchableOpacity>
     </View>
-    <View>
-      <Text>X</Text>
-    </View>
-  </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-   memoListItem: {
-    backgroundColor: '#fff',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  memoListItem: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    justifyContent: "space-between",
     paddingVertical: 16,
     paddingHorizontal: 19,
-    alignItems: 'center',
+    alignItems: "center",
     borderBottomWidth: 1,
-    borderColor: 'rgba(0,0,0,0.15)',
+    borderColor: "rgba(0,0,0,0.15)",
   },
   memoListItemTitle: {
     fontSize: 16,
@@ -32,8 +34,8 @@ const styles = StyleSheet.create({
   memoListItemDate: {
     fontSize: 12,
     lineHeight: 16,
-    color: '#848484'
-  }
-})
+    color: "#848484",
+  },
+});
 
-export default MemoListItem
+export default MemoListItem;
