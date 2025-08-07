44e4af88af7b353ff34bb86754ad34972392ffc5
TakanoriTatebe <takanori.tatebe.0727@gmail.com>
2025-08-07 11:36:41 +0900

    Create memo detail

--- src/components/CircleButton.tsx
+++ src/components/CircleButton.tsx
@@ -1,40 +1,41 @@
-import { StyleSheet, Text, View} from 'react-native'
+import { StyleSheet, Text, View, type ViewStyle } from "react-native";
 
 interface Props {
-  children: string
+  children: JSX.Element;
+  style?: ViewStyle;
 }
 
 const CircleButton = (props: Props): JSX.Element => {
-  const { children } = props
+  const { children, style } = props;
   return (
-    <View style={styles.circleButton}>
+    <View style={[styles.circleButton, style]}>
       <Text style={styles.circleButtonLabel}>{children}</Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   circleButton: {
     width: 64,
     height: 64,
     borderRadius: 32,
-    backgroundColor: '#467fd3',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
+    backgroundColor: "#467fd3",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
     right: 40,
     bottom: 40,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOpacity: 0.25,
     shadowRadius: 8,
     shadowOffset: { width: 0, height: 8 },
-    elevation: 8
+    elevation: 8,
   },
   circleButtonLabel: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 40,
-    lineHeight: 48
-  }
-})
+    lineHeight: 48,
+  },
+});
 
-export default CircleButton
+export default CircleButton;
