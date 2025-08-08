99d5b645a9d6fc825263940417b92adf5d51fcb1
TakanoriTatebe <takanori.tatebe.0727@gmail.com>
2025-08-08 16:31:10 +0900

    Introduce Stack nav

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -8,7 +8,6 @@ import {
 
 import { Link, router } from "expo-router";
 
-import Header from "../../components/Header";
 import Button from "../../components/Button";
 
 const handlePress = (): void => {
@@ -19,7 +18,6 @@ const handlePress = (): void => {
 const SignUp = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <Header />
       <View style={styles.inner}>
         <Text style={styles.title}>Sign Up</Text>
         <TextInput style={styles.input} value="Email address"></TextInput>
